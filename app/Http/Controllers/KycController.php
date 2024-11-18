a4ed8199818e230b1dfa675e883a9517e66bfafe
Alireza <alireza2000sajedi@gmail.com>
2024-11-18 12:41:32 +0330

    Refactor: KycService.php

--- app/Http/Controllers/KycController.php
+++ app/Http/Controllers/KycController.php
@@ -4,22 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreKycRequest;
 use App\Http\Resources\KycResource;
+use App\Services\FileStorageService;
 use App\Services\KycService;
 use Ars\Responder\Facades\Responder;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class KycController extends Controller
 {
     private KycService $kycService;
+    private FileStorageService $fileStorageService;
 
-    public function __construct(KycService $kycService)
+    public function __construct(KycService $kycService, FileStorageService $fileStorageService)
     {
         $this->kycService = $kycService;
+        $this->fileStorageService = $fileStorageService;
     }
 
     public function store(StoreKycRequest $request): JsonResponse
     {
-        $result = $this->kycService->storeKyc($request);
+        $result = DB::transaction(function () use ($request) {
+            $nationalCode = $request->get('national_code');
+            $birthDate = $request->get('birth_date');
+            //Save file
+            $path = $this->fileStorageService->storeImage($request->file('selfie_image'), $nationalCode);
+
+            return $this->kycService->storeKyc([
+                'national_code'     => $nationalCode,
+                'birth_date'        => $birthDate,
+                'selfie_image_path' => $path,
+            ]);
+        });
+
 
         if (!$result) {
             return Responder::internalError();
--- app/Services/KycService.php
+++ app/Services/KycService.php
@@ -2,11 +2,8 @@
 
 namespace App\Services;
 
+use App\Models\Kyc;
 use App\Repositories\KycRepository;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Database\Eloquent\Model;
 
 class KycService
 {
@@ -19,31 +16,12 @@ class KycService
         $this->fileStorageService = $fileStorageService;
     }
 
-    public function storeKyc(Request $request): ?Model
+    public function storeKyc(array $data): ?Kyc
     {
-        DB::beginTransaction();
-        try {
-            $path = $this->fileStorageService->storeImage($request->file('selfie_image'), $request->national_code);
-
-            $kyc = $this->kycRepository->storeKycData([
-                'national_code' => $request->national_code,
-                'birth_date' => $request->birth_date,
-                'selfie_image_path' => $path,
-            ]);
-
-            DB::commit();
-
-            return $kyc;
-        } catch (\Exception $e) {
-            DB::rollBack();
-
-            Log::error($e);
-
-            return null; // Return null instead of false for better type consistency
-        }
+        return $this->kycRepository->storeKycData($data);
     }
 
-    public function getKyc(string $nationalCode): Model
+    public function getKyc(string $nationalCode): ?Kyc
     {
         return $this->kycRepository->findByNationalCodeOnCache($nationalCode);
     }
